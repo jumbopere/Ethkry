fd3a594cac9f3e43b0aac9db6e68697558bbd91c
jumbo pere <jumboperebara0@gmail.com>
2022-01-09 13:13:49 +0100

    third commit

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,4 +2,4 @@ import abi from './Transactions.json';
 
 export const contractABI = abi.abi
 
-export const contractAddress = " 0x406d57c883381a78b67D0da673F076dc3500cce5";
+export const contractAddress = "0xA69eF5F7727A3876345f416dF17Ab2ee34007B33";
